4671034e215c58ae6f5d1d7728a7a9a79acb757a
Pete Tynan <pete.tynan22@gmail.com>
2016-09-04 08:32:19 +0100

    commit 2

--- Xcode course 1/ViewController.swift
+++ Xcode course 1/ViewController.swift
@@ -3,16 +3,31 @@
 //  Xcode course 1
 //
 //  Created by Pete Tynan on 21/08/2016.
-//  Copyright © 2016 Pete Tynan. All rights reserved.
+//  Copyright © 2016 Pete Tynan. All rights reserved....
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 15 {
+            theLabel.text = "You tapped the button 15 times!"
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib....
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
